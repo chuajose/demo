686e7014db97e81d18d540a3a3705af56607425f
Jose Manuel Suarez Bravo <chua.jose@gmail.com>
2019-06-10 15:31:21 +0200

    start

---
+++ src/main/java/com/example/demo/Greeting.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+    private final String pepe;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+        this.pepe = "sfsaf";
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getPepe() {
+        return pepe;
+    }
+}
